f4a1c90af2e69915f1f07a4db0a953cccae9f249
Olga Zhabko <102178041+OlgaZhabko@users.noreply.github.com>
2023-09-18 21:01:56 +0300

    MemberClass.py created

---
+++ memberclass.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3.8
+from datetime import datetime, date
+
+class PartyMember:
+  def __init__(self, name, age, country, city, occupation):
+    self.name = name
+    self.age = age
+    self.country = country
+    self.city = city
+    self.occupation = occupation
+
+def add_interest(self, interest):
+  self.interest = interest
+
+def add_to_party(self):
+  self.date_ent = datetime.now().date()
